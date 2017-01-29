c6549c0e2f4b6f6e593c6ded24076842e2da1423
Riza Fahmi <rizafahmi@gmail.com>
2017-01-29 15:57:15 +0700

    Hello world component

---
+++ src/components/CulinaryList.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const CulinaryList = (props) => {
+  return (
+    <span>Makanan Indonesia</span>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { CulinaryList } from './components/CulinaryList';
 
 ReactDOM.render(
-  <App />,
+  <CulinaryList />,
   document.getElementById('root')
 );
